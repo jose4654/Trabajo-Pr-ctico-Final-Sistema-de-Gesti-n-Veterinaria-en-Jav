b39653dae7f23882a7c98c4b135d4dbb63069665
guiza <armando503jose@gmail.com>
2023-11-28 18:45:51 -0300

    tp

---
+++ Veterinaria/src/main/java/com/arg/programa40/Veterinaria/VeterinariaApplication.java
@@ -0,0 +1,13 @@
+package com.arg.programa40.Veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaApplication.class, args);
+	}
+
+}
---
+++ Veterinaria/src/test/java/com/arg/programa40/Veterinaria/VeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arg.programa40.Veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
